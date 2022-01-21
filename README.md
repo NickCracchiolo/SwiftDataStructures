fb139e2770f697d0ebaa1e07a2ec0b7ca7ee739d
Nick Cracchiolo <cracchiolonick@outlook.com>
2022-01-20 22:28:58 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NicksDataStructures
+
+A description of this package.
---
+++ Sources/NicksDataStructures/NicksDataStructures.swift
@@ -0,0 +1,6 @@
+public struct NicksDataStructures {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
